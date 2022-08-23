a560618994d4a95332aaca506e01daca547eb344
Van-s <45581212+Van-s@users.noreply.github.com>
2022-08-22 21:47:56 -0600

    saved over

---
+++ Sum of odd numbers.js
@@ -0,0 +1,10 @@
+// P: Int
+// R: Int
+// E: rowSumOddNumbers(1) -> 1
+// rowSumOddNumbers(42) -> 74088
+// P: mathimatically it comes out to n**3
+
+
+function rowSumOddNumbers(n) {
+  return n**3
+}
