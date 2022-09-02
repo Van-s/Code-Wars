a21e80a69c4a9a7412fef7f8fd17beece51b778f
Van-s <45581212+Van-s@users.noreply.github.com>
2022-09-02 11:16:29 -0600

    added map to are they the same

--- Are they the same.js
+++ Are they the same.js
@@ -18,3 +18,16 @@ function comp(array1, array2){
   }
 
 }
+
+
+//wanted to try using map and works much cleaner
+
+function comp(array1, array2){
+  if(array1 === [] || array2 === [] || array1 === null || array2 === null){
+    return false
+  } else {
+  let arr = array2.map(i => Math.sqrt(i))
+  return arr.sort().every((val, index) => val === array1.sort()[index])
+  }
+
+}
