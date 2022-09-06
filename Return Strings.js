fd9666ebee9af7d1ea3746a5b4f26219515409aa
Van-s <45581212+Van-s@users.noreply.github.com>
2022-09-06 08:37:48 -0600

    return strings

---
+++ Return Strings.js
@@ -0,0 +1,11 @@
+// input str
+// output str with str
+// "Ryan" -> "Hello Ryan how are you doing today?"
+// use back ticks to include input
+
+
+
+
+function greet(name){
+  return (`Hello, ${name} how are you doing today?`)
+}
