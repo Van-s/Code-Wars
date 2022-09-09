c3b9ce2e64c8eb96580e128316ffc0e979bbeedf
Van-s <45581212+Van-s@users.noreply.github.com>
2022-09-09 16:22:42 -0600

    adding grasshoppper

---
+++ Grasshopper check for factor.js
@@ -0,0 +1,10 @@
+// input int and int
+// output boolean
+// checkForFactor(10,2) -> true
+// checkForFactor(63,7) -> true
+// module base by factor and compare to zero 
+
+
+function checkForFactor (base, factor) {
+  return base%factor === 0
+}
