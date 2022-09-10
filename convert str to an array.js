1bf2c7c1b51920d60f48fd4dc704d01270a99e67
Van-s <45581212+Van-s@users.noreply.github.com>
2022-09-10 11:37:58 -0600

    adding str to arr

---
+++ convert str to an array.js
@@ -0,0 +1,9 @@
+// input str
+// output arr
+// ("Robin Singh") -> ["Robin", "Singh"]
+// ("I love arrays they are my favorite") -> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+// split the str will return arr
+
+function stringToArray(string){
+  return string.split(' ')
+}
