cd4735beb6c583bdac78162b72e95629d83bd3dd
Van-s <45581212+Van-s@users.noreply.github.com>
2022-10-12 19:37:40 -0600

    adding drink about

---
+++ drink about.js
@@ -0,0 +1,17 @@
+// input int
+// output str depending on input
+// 
+// create if statement for each condition
+
+
+function peopleWithAgeDrink(old) {
+  if(old >= 21){
+    return "drink whisky"
+  } else if (old >= 18){
+    return "drink beer"
+  }else if (old >= 14){
+    return "drink coke"
+  }else{
+    return "drink toddy"
+  }
+};
